6bcfcbdce6f57c712ebe67066c702209d10badaa
rderbier <rderbier@gmail.com>
2025-08-29 18:42:18 -0700

    latest state

--- aws-lambda-classifier/sagemaker/deploys3.sh
+++
@@ -1,2 +0,0 @@
-tar -C model -zcvf model.tar.gz .
-aws s3 cp ./model.tar.gz s3://hm-sentence-transformers
--- aws-lambda-transformer/sagemaker/deploys3.sh
+++
@@ -1,2 +0,0 @@
-tar -C model -zcvf sentence-transformer-all-minilm-l6-v2.tar.gz .
-aws s3 cp ./sentence-transformer-all-minilm-l6-v2.tar.gz s3://hm-sentence-transformers --profile marketing
